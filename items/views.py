88982d74ef94c6bb6ba1f6d9aef8b054d292a29f
abdulrahman_wael <abdelrahman.wael111@gmail.com>
2026-09-22 22:28:02 +0300

    add dashboard app + edit and delete functionality for my items

--- items/views.py
+++ items/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 def details(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -28,4 +28,31 @@ def new(request):
     return render(request, 'items/form.html', {
         'form': form,
         'title': 'New Item'
+    })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk):
+
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('items:details', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'items/form.html', {
+        'form': form,
+        'title': 'Edit Item'
     })
--- puddle/settings.py
+++ puddle/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'items',
+    'dashboard',
+    
 ]
 
 MIDDLEWARE = [
