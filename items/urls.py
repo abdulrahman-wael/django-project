da7ef26b40767a182bc871e47eab3310bf203f47
abdulrahman_wael <abdelrahman.wael111@gmail.com>
2026-09-24 15:20:27 +0300

    add Browse items page html, views.py ... and link them to the base.html and urls

--- items/urls.py
+++ items/urls.py
@@ -5,6 +5,7 @@ app_name = 'items'
 
 urlpatterns = [
     path('new/', views.new, name='new'),
+    path('', views.browse, name='browse'),
     path('<int:pk>', views.details, name='details'),
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('<int:pk>/edit/', views.edit, name='edit'),
